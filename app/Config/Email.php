e2ba6462026eb78a6df78cca5f0901e4e5573c0d
brunocriacoes <br.rafael@outlook.com>
2023-11-28 16:45:32 -0300

    add sistema de login e recuperação de senha

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'contato@doarsandbox.com.br';
+    public string $fromName   = 'Doar Digital';
     public string $recipients = '';
 
     /**
